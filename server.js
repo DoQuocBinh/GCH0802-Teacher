16645c849f8a795aab4a46c33649f6e96d8553ad
Binh Do <binhdq@fe.edu.vn>
2021-03-18 16:46:37 +0700

    readfile

---
+++ server.js
@@ -0,0 +1,37 @@
+var http = require('http'); // Import Node.js core module
+const fs = require('fs')
+
+var server = http.createServer(function (req, res) {   //create web server
+    if (req.url == '/') { //check the URL of the current request       
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });   
+        let content = fs.readFileSync('hello.txt','utf8')    
+        let names = content.split('\n');
+        let list = '<ol>';
+        for(i=0;i<names.length;i++){
+            list += '<li>' + names[i] + '</li>';
+        }  
+        list += '</ol>';
+        // set response content  
+        let a1 = '<br><a href="/student">Student</a>';  
+        let a2 = '<br><a href="/admin">Admin</a>';  
+        res.write(`<html><body><p style="color:red">This is home Page 123.</p>${a1} ${a2} ${list}</body></html>`);
+        res.end();    
+    }
+    else if (req.url == "/student") {      
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();   
+    }
+    else if (req.url == "/admin") {       
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();   
+    }
+    else
+        res.end('Invalid Request!');
+});
+
+server.listen(5000); //6 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
