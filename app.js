cd01fc9a90c68e6eb98cfb4ed37598e21c06330f
Binh Do <binhdq@fe.edu.vn>
2021-03-18 15:50:48 +0700

    aa

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const circle = require('./circle.js')
 var s = 'Hello world'
 
 //console.log("Dien tich hinh tron co r =4: " +circle.area(4));
-var r =4;
-console.log(`Dien tich hinh tron co r=${r} la: ${circle.area(r)}`)
-console.log(`\nChu vi hinh tron co r= ${r}la: ${circle.circumference(r)}`)
+// var r =4;
+// console.log(`Dien tich hinh tron co r=${r} la: ${circle.area(r)}`)
+// console.log(`\nChu vi hinh tron co r= ${r}la: ${circle.circumference(r)}`)
+// console.log('dien tich chu nhat: '+circle.cvcn(70,40))
 
-
-for(i=0;i<10;i++){
-    console.log(s.toUpperCase());
-}
+const fs = require('fs')
+//Doc khong dong bo: Async
+// fs.readFile('hello.txt','utf8',function(error,data){
+//     if(error)
+//         console.log('File khong truy cap duoc')
+//     else{
+//         console.log(data)
+//     }
+// })
+// console.log('123')
+//doc dong bo(Sync)
+var content =fs.readFileSync('hello.txt','utf-8')
+console.log('456')
+console.log(content)
--- circle.js
+++ circle.js
@@ -1,2 +1,8 @@
 exports.area = (r)=> Math.PI*r**2;
-exports.circumference = (r)=> 2 *Math.PI*r;
+
+exports.circumference = function(r) {
+    return 2 *Math.PI*r;
+}
+exports.cvcn = (d,r)=> {
+    return d*r;
+}
